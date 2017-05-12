9c12eaccf810bf0e60bd251f28fbe01321d17d74
Chris Carr <coder.knot@gmail.com>
2017-05-12 12:27:11 -0700

    add route for about page

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,16 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutComponent } from './about/about.component';
 
 const appRoutes: Routes = [
 	{
 	  path: '',
 	  component: LandingPageComponent
+	},
+	{
+		path: 'about',
+		component: AboutComponent
 	}
 ];
 
