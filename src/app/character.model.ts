920b08d4cffb5fdf5c04922b7bc8ce42be42e2e8
Chris Carr <coder.knot@gmail.com>
2017-05-12 14:04:36 -0700

    add charachter model

---
+++ src/app/character.model.ts
@@ -0,0 +1,7 @@
+export class Character {
+  constructor(public name: string,
+              public race: string,
+              public characterClass: string,
+              public level: number,
+              public xp: number) { }
+}
