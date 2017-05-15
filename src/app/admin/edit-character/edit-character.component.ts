4a0c290f7624d93fe18abf9dfa3b91e2be78f346
Chris Carr <coder.knot@gmail.com>
2017-05-14 21:33:20 -0700

    add functionality to update character

--- src/app/admin/edit-character/edit-character.component.ts
+++ src/app/admin/edit-character/edit-character.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { Character } from '../../character.model';
+import { CharacterService } from '../../character.service';
 
 @Component({
   selector: 'app-edit-character',
   templateUrl: './edit-character.component.html',
-  styleUrls: ['./edit-character.component.css']
+  styleUrls: ['./edit-character.component.css'],
+  providers: [CharacterService]
 })
-export class EditCharacterComponent implements OnInit {
 
-  constructor() { }
+export class EditCharacterComponent implements OnInit {
+  @Input() selectedCharacter;
+  @Output() editDoneSender = new EventEmitter();
+
+  constructor(private characterService: CharacterService) { }
 
   ngOnInit() {
+  }
+
+  beginUpdatingCharacter(characterToUpdate) {
+    this.characterService.updateCharacter(characterToUpdate);
+    this.editDoneSender.emit();
   }
 
 }
